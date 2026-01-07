fb1a9b6f3b79d1f8ebac261fdbbcad11d9cf30b5
ayayassin0923-pixel <ayayassin09.23@gmail.com>
2026-01-07 10:22:00 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// When the "Start Your Journey" button is clicked, go to the next page
+document.getElementById('startBtn').addEventListener('click', () => {
+  // For now, just alert — later we will add real navigation
+  alert('Let’s start your journey! Next, we will ask you some questions.');
+  
+  // In the future: window.location.href = 'page2.html';
+});
